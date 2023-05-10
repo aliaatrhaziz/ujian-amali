920476cf3c3669dce92e23583bb7994cce9dcdc4
aliaatirah <108789742+aliaatrhaziz@users.noreply.github.com>
2023-05-10 12:19:28 +0800

    Create file maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maintenance</title>
+</head>
+<body>
+    <h1 style="text-align: center;">SORRY! UNDER MAINTENANCE!!</h1>
+    <p style="text-align: center;">Return to <a href="index.php">Menu</a><p>
+</body>
+</html>
